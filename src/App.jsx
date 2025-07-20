7e8f1be864780f684fc33a40ffd37f5b37d38921
code-with-hajra <hajransaleem87@gmail.com>
2025-07-20 07:18:31 -0700

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 
 import './App.css'
-import Home from './home/home'
+import Home from './home/Home'
 import { Navigate, Route, Routes } from "react-router-dom"
 import Courses from './courses/courses'
 import { Toaster } from 'react-hot-toast'
@@ -16,7 +16,6 @@ function App() {
         <Route path='/' element={<Home />} />
         <Route path='/course' element={authUser ? <Courses /> : <Navigate to="/signup" />} />
         <Route path='/signup' element={<Signup />} />
-
       </Routes>
       <Toaster />
     </>
