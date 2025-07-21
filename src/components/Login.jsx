a5f191c99cff32d5a7942ed290ce1677f93d6673
code-with-hajra <hajransaleem87@gmail.com>
2025-07-21 11:38:10 +0500

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -16,7 +16,7 @@ function Login() {
       password: data.password,
     };
     try {
-      const res = await axios.post("http://localhost:4001/user/login", userInfo);
+      const res = await axios.post("https://vercel-backend-mu-flax.vercel.app/user/login", userInfo);
       if (res.data) {
         toast.success("Logged in Successfully");
         
